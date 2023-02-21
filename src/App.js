29983d8f2f031c146fa70f4b626feafb7736167d
YSHyeonn <dbstjdgus0@sogang.ac.kr>
2023-02-22 04:07:57 +0900

    result page maded (except pic)

--- src/App.js
+++ src/App.js
@@ -12,6 +12,11 @@ import Main from './pages/main_page/Main';
 import Apply from './pages/apply_page/Apply';
 import Introduce from './pages/doing_page/Introduce';
 import { Login } from './pages/login_page/Main';
+import Doc_result_fail from './pages/result_page/Doc_result_fail_page/Doc_result_fail';
+import Doc_result_pass from './pages/result_page/Doc_result_pass_page/Doc_result_pass';
+import In_result_fail  from './pages/result_page/Interview_result_fail_page/In_result_fail';
+import In_result_pass from './pages/result_page/Interview_result_pass_page/In_result_pass';
+
 
 const App = () => {
   return (
@@ -22,7 +27,12 @@ const App = () => {
         <Route path="/introduce" element={<Introduce />} />
         {/* apply페이지 경로 -> <Login /> */}
         <Route element={<ApplyBanner />} >
+
           <Route path="/apply" element={<Apply />} />
+          <Route path="result/docfail" element={<Doc_result_fail/>} />
+          <Route path="result/docpass" element={<Doc_result_pass/>} />
+          <Route path="/result/interviewpass" element={<In_result_pass/>} />
+          <Route path="/result/interviewfail" element={<In_result_fail/>} />
         </Route>
       </Route>
     </Routes>
