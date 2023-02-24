73ad9595ae47709f758b3780f613fd7e83fb32f9
ljy2855 <ljy2855@naver.com>
2023-02-24 19:59:46 +0900

    fix: Fix resume api auth header

--- src/apis/resume.js
+++ src/apis/resume.js
@@ -1,19 +1,19 @@
 import { Client } from "./client";
 import { get_auth_header } from "hooks/check_login";
 export const get_resume = async () => {
-    const response = await Client.get('/apply/resume')
+    const response = await Client.get('/apply/resume',get_auth_header())
     console.log(response.status)
     return response.data
 }
 
 export const post_resume = async (data) => {
-    const response = await Client.post('/apply/resume',data)
+    const response = await Client.post('/apply/resume',get_auth_header(),data)
     console.log(response.status)
     return response.data
 }
 
 export const update_resume = async (data) => {
-    const response = await Client.patch('/apply/resume',data)
+    const response = await Client.patch('/apply/resume',get_auth_header(),data)
     console.log(response.status)
     return response.data
 }
